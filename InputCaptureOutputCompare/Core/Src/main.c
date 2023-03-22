d4b64a9d9445e982b9f98515cde7141ed899b72c
Aekasit0075 <aekasit2002@gmail.com>
2023-03-22 21:06:56 +0700

    LAB3

--- InputCaptureOutputCompare/Core/Src/main.c
+++ InputCaptureOutputCompare/Core/Src/main.c
@@ -21,6 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+// เอกสิทธิ์ มาคำ
 #define IC_BUFFER_SIZE 20
 /* USER CODE END Includes */
 
